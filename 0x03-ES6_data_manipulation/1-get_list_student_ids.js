9dc241f093c0cfc2fa51f9e2b43ab39cdb4b18fb
Lagat100 <weldonkiprop1@gmail.com>
2024-07-04 21:42:59 +0000

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,16 @@
-function getListStudentIds(students) {
-    // Check if the input is an array, if not return an empty array
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    
-    // Use the map function to extract the ids from the list of students
-    return students.map(student => student.id);
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
